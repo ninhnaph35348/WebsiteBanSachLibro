855ddf1a53b54cf624ec7f4091c1972dd354fc05
minhphongg159 <minhgnohpp2004@gmail.com>
2025-03-06 03:04:24 +0700

    api product_variant

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductVariantController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\CategoryController;
@@ -53,3 +54,4 @@ Route::apiResource('genres', GenreController::class);
 Route::apiResource('products', ProductController::class);
 Route::apiResource('users', UserController::class);
 Route::apiResource('vouchers', VoucherController::class);
+Route::apiResource('product_variants', ProductVariantController::class);

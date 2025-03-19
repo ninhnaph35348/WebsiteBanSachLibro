e1456e481af678856e7164df4747908d307a0a3a
thangcph44444 <congthangdajk@gmail.com>
2025-03-19 19:03:16 +0700

    update

--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -15,7 +15,9 @@ class ReviewResource extends JsonResource
             'review' => $this->review,
             'status' => $this->status,
             'title' => $this->product ? $this->product->title : null,
+
             'username' => $this->user ? $this->user->username : null,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
         ];
     }
 }

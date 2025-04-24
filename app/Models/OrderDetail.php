cf5d6f65e71e2a7a319d973c0a9a973f3eda368d
ninhnaph35348 <bombimquabombim12@gmail.com>
2025-04-24 13:03:35 +0700

    fix bug

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -15,7 +15,9 @@ class OrderDetail extends Model
         'order_id',
         'product_variant_id', // Đổi từ variant_id nếu cần
         'quantity',
-        'price',
+        'total_line',
+        'hard_products', // Lưu tên sản phẩm cứng
+        'hard_price_time', // Lưu thời gian của giá cứng
     ];
 
     public $timestamps = false;

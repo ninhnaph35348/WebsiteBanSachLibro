bae31a256d975fe5f05777431f29f4f6d78efc37
ninhnaph35348 <bombimquabombim12@gmail.com>
2025-05-04 14:28:11 +0700

    posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::get();
+        $posts = Post::orderBy('id', 'desc')->get();
 
         return response()->json($posts);
     }
